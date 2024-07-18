cc684c02fb2f32db5bee313027f2193500617460
= <=>
2024-07-18 23:32:18 +0500

    font changed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,13 @@ const Navbar = () => {
       <Link href='/' className='text-2xl font-bold'>
         Tours
       </Link>
-      <div className='flex gap-5 text-semibold'>
-        <Link href='/'>Home</Link>
-        <Link href='/tours'>create tour</Link>
+      <div className='flex gap-5 capitalize'>
+        <Link href='/' className='font-semibold'>
+          Home
+        </Link>
+        <Link href='/tours' className='font-semibold'>
+          createtour
+        </Link>
       </div>
       <ModeToggle />
     </div>
