eebbe5f9da919d6da096fa443b3b44d3d9e3848e
= <=>
2024-07-18 23:27:26 +0500

    commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,9 @@ import ModeToggle from "./ModeToggle";
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center py-2'>
-      <Link href='/'>Logo</Link>
+      <Link href='/' className='text-2xl font-bold'>
+        Tours
+      </Link>
       <div className='flex gap-5 text-semibold'>
         <Link href='/'>Home</Link>
         <Link href='/tours'>Tours</Link>
